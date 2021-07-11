83787cd38aa2dedd481744afdcdcc7d2cd7f6292
kwas <kwas@kwas-MS-7641>
2021-07-11 10:31:34 +0300

    1.1_Basics_code_development

---
+++ Task_1/src/com/epam/training/fun/Main.java
@@ -0,0 +1,27 @@
+package com.epam.training.fun;
+
+import java.util.Scanner;
+
+//Find value of function
+public class Main {
+	public static void main(String[] args) {
+		double a, b, c, z;
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("type value of A\n>>");
+		a = Typing(sc1);
+		System.out.println("type value of B\n>>");
+		b = Typing(sc1);
+		System.out.println("type value of C\n>>");
+		c = Typing(sc1);
+		z = ((a - 3) * b / 2) + c;
+		System.out.println("   Z=" + z);
+	}
+
+	public static double Typing(Scanner sc) {
+		while (!sc.hasNextDouble()) {
+			System.out.print(" \"" + sc.next() + "\"");
+			System.out.print(" it is't type double\n>>");
+		}
+		return sc.nextDouble();
+	}
+}
