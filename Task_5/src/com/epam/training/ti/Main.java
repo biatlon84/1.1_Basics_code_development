699af19be525846679aa6b2f852219e1e0e4dc4e
kwas <kwas@kwas-MS-7641>
2021-07-11 12:06:35 +0300

    1.1_Basics_code_development

---
+++ Task_5/src/com/epam/training/ti/Main.java
@@ -0,0 +1,20 @@
+package com.epam.training.ti;
+
+//T is time in seconds (Task_5)
+// output T in this  view: HHч MMмин SSс 
+public class Main {
+	public static void main(String[] args) {
+		long T;
+		int h;
+		int m;
+		T = System.currentTimeMillis() / 1000;
+		// --------
+		T = 10023;
+		h = (int) (T / 3600);
+		T -= (h * 3600);
+		m = (int) (T / 60);
+		T -= m * 60;
+		System.out.println("Time formatted");
+		System.out.printf("%02dh %02dm %02ds", h, m, T);
+	}
+}
